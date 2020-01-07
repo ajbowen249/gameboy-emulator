65f2399eb81c409f59227a82117036c50cc4ae32
Alex Bowen <ajbowen249@gmail.com>
2020-01-06 20:21:41 -0500

    main loop debug tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -36,12 +36,17 @@ int main(int argc, char** args) {
 
     CPU cpu(mobo);
 
+    int iteration = 0;
     while (true) {
         cpu.clock();
 
-        // std::this_thread::sleep_for(std::chrono::nanoseconds(238));
-        std::cout << std::hex << cpu._programCounter << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::nanoseconds(238));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        if (iteration++ >= 4) {
+            std::cout << std::hex << cpu._programCounter << std::endl;
+            iteration = 0;
+        }
     }
 
     return 0;
